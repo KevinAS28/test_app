0efdbe882bc972ded2229f5e1b9d3926ff54f751
kevin <kevinagusto28@gmail.com>
2020-04-25 16:27:38 +0700

    changing manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
-import sys
-
+import numpy
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yay.settings')
